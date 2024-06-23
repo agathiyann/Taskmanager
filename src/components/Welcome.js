eb4d17122863e0b98dddd375ac7ca76297e24a7f
Agathiyan <agathib8@gmail.com>
2024-06-23 21:32:57 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { RiTaskFill } from "react-icons/ri";
+
 import './Welcome.css';
  
 const Welcome = () => {
@@ -8,7 +8,7 @@ const Welcome = () => {
       <div className="welcome-container">
         <div className="header-container">
           <h1>Task</h1>
-          <h1 className="task-icon"><RiTaskFill /></h1>
+          <h1 className="task-icon"></h1>
         </div>
         <p>Manage your tasks</p>
         <Link to="/tasks">
