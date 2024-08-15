34407027713d0524999eedc55b9c4ab32e846073
agathiyann <74144574+agathiyann@users.noreply.github.com>
2024-08-15 12:52:25 +0530

    commit

--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -3,24 +3,24 @@ import { Link, useParams } from 'react-router-dom';
 
 const TaskDetail = ({ tasks }) => {
   const { id } = useParams();
-  const task = tasks.find(task => task.id === id);
+  const task = tasks.find(task => task._id === id);  // Updated _id instead of id
 
   if (!task) {
-    return <div>Task not. found</div>;
+    return <div>Task not found</div>;
   }
 
   return (
-      <div className="task-detail">
-        <h1>{task.title}</h1>
-        <p>{task.description}</p>
-        <p>Due: {task.dueDate}</p>
-        <div>
-          <Link to={`/edit/${task.id}`} key={task.id}>Edit Task</Link>
-        </div>
-        <div>
-          <Link to="/">Back to Task List</Link>
-        </div>
+    <div className="task-detail">
+      <h1>{task.title}</h1>
+      <p>{task.description}</p>
+      <p>Due: {task.dueDate}</p>
+      <div>
+        <Link to={`/edit/${task._id}`} key={task._id}>Edit Task</Link>  {/* Updated _id instead of id */}
       </div>
+      <div>
+        <Link to="/tasks">Back to Task List</Link>
+      </div>
+    </div>
   );
 };
 
