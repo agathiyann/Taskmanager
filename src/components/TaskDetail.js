2e6b7f5a46bd51eeef0a3c188d7dbd2c85a0ced4
agathiyann <74144574+agathiyann@users.noreply.github.com>
2024-08-15 13:29:06 +0530

    commit

--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -3,19 +3,27 @@ import { Link, useParams } from 'react-router-dom';
 
 const TaskDetail = ({ tasks }) => {
   const { id } = useParams();
-  const task = tasks.find(task => task._id === id);  
+  const task = tasks.find(task => task._id === id);
 
   if (!task) {
     return <div>Task not found</div>;
   }
 
+  const formattedDueDate = new Date(task.dueDate).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
   return (
     <div className="task-detail">
       <h1>{task.title}</h1>
       <p>{task.description}</p>
-      <p>Due: {task.dueDate}</p>
+      <p>
+        Due:&nbsp;&nbsp;&nbsp;{formattedDueDate}  
+      </p>
       <div>
-        <Link to={`/edit/${task._id}`} key={task._id}>Edit Task</Link>  
+        <Link to={`/edit/${task._id}`} key={task._id}>Edit Task</Link>
       </div>
       <div>
         <Link to="/tasks">Back to Task List</Link>
